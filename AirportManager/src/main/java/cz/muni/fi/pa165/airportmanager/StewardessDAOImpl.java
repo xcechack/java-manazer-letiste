7a6e2c616a4947ea700a5b247a68db0c8cd58e3a
neudert.lukas <neudert.lukas@gmail.com>
2012-10-10 09:41:33 +0000

    vytroreni dao pro stewardess

---
+++ AirportManager/src/main/java/cz/muni/fi/pa165/airportmanager/StewardessDAOImpl.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.airportmanager;
+
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Frkal
+ */
+public class StewardessDAOImpl implements StewardessDAO {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void create(Stewardess stewardess) {
+        if (stewardess == null) {
+            throw new NullPointerException("Stewardess must not be null.");
+        }
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(stewardess);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Stewardess get(Long id) {
+        if (id == null) {
+            throw new NullPointerException("Id must not be null.");
+        }
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        Stewardess stew = em.find(Stewardess.class, id);
+        em.getTransaction().commit();
+        em.close();
+        return stew;
+    }
+
+    public void update(Stewardess stewardess) {
+        if (stewardess == null) {
+            throw new NullPointerException("Stewardess must not be null.");
+        }
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(stewardess);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void remove(Stewardess stewardess) {
+        if (stewardess == null) {
+            throw new NullPointerException("Stewardess must not be null.");
+        }
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(stewardess);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public List<Stewardess> findAll() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        List<Stewardess> list;
+        em.getTransaction().begin();
+        list = em.createQuery("SELECT * FROM Stewardess").getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return Collections.unmodifiableList(list);
+    }
+}
