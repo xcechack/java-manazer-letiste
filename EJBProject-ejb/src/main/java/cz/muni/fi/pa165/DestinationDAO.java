5a34869268adc4d436175c03be6e228b2fcb31fe
vasekjanosek <vasekjanosek@gmail.com>
2012-11-11 18:17:58 +0000

    relocating EJB project

---
+++ EJBProject-ejb/src/main/java/cz/muni/fi/pa165/DestinationDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.ejb.Local;
+/**
+ *
+ * @author Jaroslav Nespesny, 359566
+ * @mail.muni.cz
+ */
+
+@Local
+public interface DestinationDAO {
+    
+    /**
+     * Creates new destination
+     * 
+     * @param destination Destination we want to store.
+     * @throws NullPointerException when given destination is null. 
+     * @return nothing.
+     */
+    void create(Destination destination);
+    
+    
+}
