ad308b5047f9d7923b6d4c023f72d2e10e886e08
vasekjanosek@gmail.com <vasekjanosek@gmail.com@7b8e5a93-465b-6e9d-bda5-e1d260de589f>
2013-01-25 15:46:56 +0000

    implementováno UserDAOImpl

--- trunk/AirportManager/airportmanager-daos/src/main/java/cz/muni/fi/pa165/airportmanager/UserDAOImpl.java
+++ trunk/AirportManager/airportmanager-daos/src/main/java/cz/muni/fi/pa165/airportmanager/UserDAOImpl.java
@@ -5,6 +5,8 @@
 package cz.muni.fi.pa165.airportmanager;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,28 +16,61 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserDAOImpl implements UserDAO {
 
+    @PersistenceContext
+    private EntityManager em;
+    
     public void create(User user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(user == null){
+            throw new IllegalArgumentException("User must not be null");
+        }
+        if(user.getUsername() == null || user.getUsername().isEmpty()){
+            throw new IllegalArgumentException("User's name must not be null or empty");
+        }
+        
+        em.persist(user);
     }
 
     public User get(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(id == null){
+            throw new IllegalArgumentException("Id must not be null");
+        }
+        
+        User user = em.find(User.class, id);
+        
+        return user;
     }
 
     public void update(User user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(user == null || user.getId() == null){
+            throw new IllegalArgumentException("User or his id must not be null");
+        }
+        em.merge(user);
     }
 
     public void delete(User user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(user == null || user.getId() == null){
+            throw new IllegalArgumentException("User or his id must be null");
+        }
+        
+        User userToDelete = em.find(User.class, user.getId());
+        
+        em.remove(userToDelete);
     }
 
     public List<User> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<User> list;
+        list = em.createQuery("SELECT s FROM REGISTERED_USERS s").getResultList();
+        return list;
     }
 
     public User getUserByUsername(String username) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(username == null){
+            throw new IllegalArgumentException("Usename must not be null");
+        }
+        
+        User user = em.find(User.class, username);
+        
+        return user;
     }
     
 }
