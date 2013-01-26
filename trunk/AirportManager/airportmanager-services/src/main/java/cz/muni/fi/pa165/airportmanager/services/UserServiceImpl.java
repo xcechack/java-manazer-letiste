4ee380af37a081acce797846d45cd972e34b0dcb
vasekjanosek@gmail.com <vasekjanosek@gmail.com@7b8e5a93-465b-6e9d-bda5-e1d260de589f>
2013-01-26 10:56:22 +0000

    implementovano UserServiceImpl.java

--- trunk/AirportManager/airportmanager-services/src/main/java/cz/muni/fi/pa165/airportmanager/services/UserServiceImpl.java
+++ trunk/AirportManager/airportmanager-services/src/main/java/cz/muni/fi/pa165/airportmanager/services/UserServiceImpl.java
@@ -4,8 +4,18 @@
  */
 package cz.muni.fi.pa165.airportmanager.services;
 
+import cz.muni.fi.pa165.airportmanager.EntityDTOMapper;
+import cz.muni.fi.pa165.airportmanager.User;
+import cz.muni.fi.pa165.airportmanager.UserDAO;
 import cz.muni.fi.pa165.airportmanager.UserDTO;
+import cz.muni.fi.pa165.airportmanager.exceptions.DAOException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -13,20 +23,122 @@ import java.util.List;
  */
 public class UserServiceImpl implements UserService{
 
-    public void create(UserDTO user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    @Autowired
+    private UserDAO uDao;
+    
+    public void setuDao(UserDAO uDao){
+        this.uDao = uDao;
+    }
+    
+    @Transactional
+    public void create(UserDTO userDTO) {
+        if(userDTO!=null){
+            try
+            {
+                if(SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication()!= null){
+                    System.out.println("SEC CX HOLDER: "+SecurityContextHolder.getContext().getAuthentication());
+                    List<GrantedAuthority> authorities = (List<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+                    
+                    if(!authorities.contains(new SimpleGrantedAuthority("USER"))){
+                       //System.out.println("SEC CX isA");
+                       throw new DAOException("Insufficient granted authorities.");
+                    }
+                }
+                
+                User user = EntityDTOMapper.userDTOToUser(userDTO);
+                uDao.create(user);
+                userDTO.setId(user.getId());
+            }catch(DataAccessException ex)
+            {
+                //DAO operation failed
+                throw new DAOException(ex.toString());
+            }
+        }else{
+            throw new NullPointerException("Given plane was null");
+        }
+    }
+    
+    @Transactional
+    public void update(UserDTO userDTO){
+        if(userDTO!=null){
+            try
+            {
+                if(SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication()!= null){
+                    System.out.println("SEC CX HOLDER: "+SecurityContextHolder.getContext().getAuthentication());
+                    List<GrantedAuthority> authorities = (List<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+                    
+                    if(!authorities.contains(new SimpleGrantedAuthority("USER"))){
+                       //System.out.println("SEC CX isA");
+                       throw new DAOException("Insufficient granted authorities.");
+                    }
+                }
+                uDao.update(EntityDTOMapper.userDTOToUser(userDTO));
+            }catch(DataAccessException ex)
+            {
+                //DAO operation failed
+                throw new DAOException(ex.toString());
+            }
+        }else{
+            throw new NullPointerException("Stewardess must not be null.");
+        }
     }
 
-    public void delete(UserDTO user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    @Transactional
+    public void delete(UserDTO userDTO) {
+        if(userDTO!=null){
+            try
+            {
+                if(SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication()!= null){
+                    System.out.println("SEC CX HOLDER: "+SecurityContextHolder.getContext().getAuthentication());
+                    List<GrantedAuthority> authorities = (List<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+                    
+                    if(!authorities.contains(new SimpleGrantedAuthority("USER"))){
+                       //System.out.println("SEC CX isA");
+                       throw new DAOException("Insufficient granted authorities.");
+                    }
+                }
+                uDao.delete(EntityDTOMapper.userDTOToUser(userDTO));
+            }catch(DataAccessException ex)
+            {
+                //DAO operation failed
+                throw new DAOException(ex.toString());
+            }
+        }else{
+            throw new NullPointerException("Stewardess must not be null.");
+        }
     }
 
+    @Transactional
     public UserDTO get(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        UserDTO result = null;
+        if(id!=null){
+            try
+            {
+                result = EntityDTOMapper.userToUserDTO(uDao.get(id));
+            }catch(DataAccessException ex)
+            {
+                //DAO operation failed
+                throw new DAOException(ex.toString());
+            }
+        }else{
+            throw new NullPointerException("Id must not be null.");
+        }
+        return result;
     }
 
+    @Transactional
     public List<UserDTO> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<UserDTO> result = null;
+        
+        try
+        {
+            result = EntityDTOMapper.userListToUserDTOList(uDao.findAll());
+        }catch(DataAccessException ex)
+        {
+            throw new DAOException(ex.toString());
+        }
+        
+        return result;
     }
     
 }
