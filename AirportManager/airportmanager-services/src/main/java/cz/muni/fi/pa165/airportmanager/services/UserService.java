7086ca3ce7fbcd834e9ed8b7398dd39a3b8a3c7f
vasekjanosek <vasekjanosek@gmail.com>
2013-01-26 10:56:22 +0000

    implementovano UserServiceImpl.java

--- AirportManager/airportmanager-services/src/main/java/cz/muni/fi/pa165/airportmanager/services/UserService.java
+++ AirportManager/airportmanager-services/src/main/java/cz/muni/fi/pa165/airportmanager/services/UserService.java
@@ -12,13 +12,41 @@ import java.util.List;
  * @author Marek
  */
 public interface UserService {
-    
-    public void create(UserDTO user);
-    
-    public void delete(UserDTO user);
-    
-    public UserDTO get(Long id);
-    
-    public List<UserDTO> findAll();
+    /**
+     * Create new stewardess
+     * 
+     * @param userDTO user we want to store.
+     * @throws NullPointerException when argument is null.
+     * @return nothing.
+     */
+    void create(UserDTO userDTO);
+    /**
+     * Update existing user
+     * @param userDTO user we want to update
+     * @return nothing
+     * @throws NullPointerException when given stewardess doesnt exist.
+     */
+    void update(UserDTO userDTO);
+    /**
+     * Delete existing user
+     * @param userDTO user we want to delete
+     * @return nothing
+     * @throws NullPointerException when given stewardess doesnt exist.
+     */
+    void delete(UserDTO userDTO);
+    /**
+     * Returns User entity with given id.
+     * 
+     * @throws NullPointerException when id is null.
+     * @param id Id of user entity.
+     * @return user with given id or null if user doesnt exists.
+     */
+    UserDTO get(Long id);
+    /**
+     * Retrieve all users
+     * @param nothing
+     * @return List with all users.
+     */
+    List<UserDTO> findAll();
     
 }
